72bbef8ff4069f63c8e6f6b01459b99ce04e9b5e
NickDC <134907284+NickDC12@users.noreply.github.com>
2025-10-08 15:55:17 -0500

    Add files via upload

---
+++ src/main/java/edu/uca/registration/model/Course.java
@@ -0,0 +1,27 @@
+package edu.uca.registration.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Course {
+    //course identifier
+    public String code;
+
+    //Title of course
+    public String title;
+
+    //max students in course
+    public int capacity;
+
+    //list of students ID in course
+    public List<String> roster = new ArrayList<>();
+
+    //list of student IDs on waitlist
+    public List<String> waitlist = new ArrayList<>();
+
+    public Course(String code, String title, int capacity) {
+        this.code = code;
+        this.title = title;
+        this.capacity = capacity;
+    }
+}
---
+++ src/main/java/edu/uca/registration/model/Student.java
@@ -0,0 +1,21 @@
+package edu.uca.registration.model;
+
+public class Student {
+
+    //student ID
+    public String id;
+    // student full name
+    public String name;
+    // student email address
+    public String email;
+
+    public Student(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    @Override
+    public String toString() {
+        return id + " " + name + " <" + email + ">";
+    }
+}
