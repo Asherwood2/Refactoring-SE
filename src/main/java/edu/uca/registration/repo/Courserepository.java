7be0e6296880989f59afaf63070d4cff13fae9d3
Austin Sherwood <asherwood349@cub.uca.edu>
2025-10-08 18:40:42 -0500

    added course repo contents

--- src/main/java/edu/uca/registration/repo/Courserepository.java
+++ src/main/java/edu/uca/registration/repo/Courserepository.java
@@ -4,10 +4,22 @@
  */
 package edu.uca.registration.repo;
 
-/**
- *
- * @author Austin
- */
+import edu.uca.registration.model.Course;
+import java.util.*;
+
+
 public class Courserepository {
+    private Map<String, Course> courses = new LinkedHashMap<>();
     
+    public void add(Course c){
+        courses.put(c.getCode(), c);
+    }
+    
+    public Course get(String code){
+        return courses.get(code);
+    }
+    
+    public Collection<Course> getAll(){
+        return courses.values();
+    }
 }
