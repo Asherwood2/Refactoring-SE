b044bd2948fa20d6441eccfd9313b82d426a9107
NickDC <134907284+NickDC12@users.noreply.github.com>
2025-10-28 18:13:52 -0500

    Add unit tests for input validation

---
+++ src/test/java/edu/uca/registration/ValidatorTest.java
@@ -0,0 +1,47 @@
+package edu.uca.registration;
+
+import edu.uca.registration.exception.ValidationException;
+import edu.uca.registration.util.Validator;
+import org.junit.Test;
+
+public class ValidatorTest {
+    
+    @Test
+    public void testValidBannerId() {
+        // Should not throw exception
+        Validator.validateBannerId("B001");
+        Validator.validateBannerId("B123");
+    }
+    
+    @Test(expected = ValidationException.class)
+    public void testInvalidBannerId() {
+        Validator.validateBannerId("invalid");
+    }
+    
+    @Test
+    public void testValidEmail() {
+        Validator.validateEmail("test@uca.edu");
+    }
+    
+    @Test(expected = ValidationException.class)
+    public void testInvalidEmail() {
+        Validator.validateEmail("notanemail");
+    }
+    
+    @Test
+    public void testValidCapacity() {
+        Validator.validateCapacity(1);
+        Validator.validateCapacity(50);
+        Validator.validateCapacity(500);
+    }
+    
+    @Test(expected = ValidationException.class)
+    public void testInvalidCapacityTooLow() {
+        Validator.validateCapacity(0);
+    }
+    
+    @Test(expected = ValidationException.class)
+    public void testInvalidCapacityTooHigh() {
+        Validator.validateCapacity(501);
+    }
+}
